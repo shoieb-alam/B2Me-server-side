44a90be4e37efb59e03cadab5755d544f3942149
sheikh ariful <sheikharifulislam770@gmail.com>
2022-01-01 21:41:52 +0600

    second commit

---
+++ app.js
@@ -0,0 +1,82 @@
+const express  = require('express');
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv').config()
+const objectId = require('mongodb').ObjectId;
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.jrudo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("B2Me");
+        const allProduct = database.collection("All_Product");
+
+        //All Get Api
+        
+        app.get('/', async (req, res) => {
+            res.send("Well Come")
+            const users = {
+                name: "Ariful",
+                age: 16,
+            }
+        
+            await allProduct.insert(users);
+        
+        
+        });
+        
+        
+
+        app.get('/all-products', async (req, res) => {
+           const {category} = req.query;
+           const {productId} = req.query;
+           const {productName} = req.query;
+
+           let products;
+
+           if(category !== true && productId !== true && productName !== true) {
+                products = await allProduct.find({}).toArray();
+                res.send(products);
+           }
+           else if(category) {
+                products = await allProduct.find({category: category}).toArray();
+                res.send(products);
+           }
+           else if(productId) {
+               products = await allProduct.find({_id: objectId(productId)});
+               res.send(products);
+           }
+           else if(productName) {
+               products = await allproduct.find({productName: productName}).toArray();
+           }
+           
+           
+        })
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+    finally {
+        // await client.close();
+    }
+
+    
+}
+
+run();
+
+
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server is running at port ${port}`);
+});
